13fe2933674198f317029a64051dc0dd52eae445
myOmikron <git@omikron.dev>
2022-07-01 02:58:31 +0200

    Added validation checks

--- source/dorm/migration/package.d
+++ source/dorm/migration/package.d
@@ -109,6 +109,217 @@ Migration[] getExistingMigrations(ref MigrationConfig conf)
 }
 
 /** 
+ * Validate the given migrations in terms of their set
+ * dependencies, replaces and initial values
+ *
+ * Params:
+ *   existing = Reference to an array of Migrations
+ *
+ * Throws:
+ *   dorm.exceptions.MigrationException
+ */
+void validateMigrations(ref Migration[] existing)
+{
+    Migration[string] lookup;
+
+    // Build lookup table
+    foreach (migration; existing)
+    {
+        // If an id is existing more than once, throw
+        if ((migration.id in lookup) !is null)
+        {
+            throw new MigrationException(
+                "Migration id found multiple times: " ~ migration.id
+            );
+        }
+
+        lookup[migration.id] = migration;
+    }
+
+    // dfmt off
+    // Check that all dependencies && replaces exist
+    existing.each!((Migration x) {
+        x.dependencies.each!((string y) {
+            if ((y in lookup) is null)
+            {
+                throw new MigrationException(
+                    "Dependency of migration " ~ x.id ~ " does not exist"
+                );
+            }
+        });
+
+        x.replaces.each!((string y) {
+            if ((y in lookup) is null)
+            {
+                throw new MigrationException(
+                    "Replaces of migration " ~ x.id ~ " does not exist"
+                );
+            }
+        });
+    });
+
+    // dfmt on
+
+    // If there are no migrations, there's nothing to do
+    if (existing.length == 0)
+        return;
+
+    // Check if migration that has initial = false has no dependencies
+    existing.each!((Migration x) {
+        if (!x.initial && x.dependencies.length == 0)
+        {
+            throw new MigrationException(
+                "No dependencies found on migration with initial = false: "
+                ~ x.id
+            );
+        }
+    });
+
+    // Check if replaces or dependencies create a loop
+    string[] visited;
+
+    void checkLoop(Migration m)
+    {
+        // If current id is in visited, that's a loop
+        if (visited.canFind(m.id))
+        {
+            throw new MigrationException(
+                "Detected loop in replaces: " ~ join(visited, " ")
+            );
+        }
+
+        visited ~= m.id;
+
+        m.replaces.each!(x => checkLoop(lookup[x]));
+
+    }
+
+    auto replaceMigrations = existing.filter!(x => x.replaces.length > 0);
+    foreach (key; replaceMigrations)
+    {
+        visited = [];
+        checkLoop(key);
+    }
+
+    auto dependencyMigrations = existing.filter!(
+        x => x.dependencies.length > 0
+    );
+    foreach (key; dependencyMigrations)
+    {
+        visited = [];
+        checkLoop(key);
+    }
+
+    // Check if none or more than one migration has inital set to true
+    auto initialMigrations = existing.filter!(x => x.initial);
+
+    // If any migrations with initial = true, throw
+    initialMigrations.each!((Migration x) {
+        if (x.dependencies.length != 0)
+        {
+            throw new MigrationException(
+                "Migration with initial = true cannot have dependencies: "
+                ~ x.id
+            );
+        }
+    });
+
+    if (initialMigrations.count() == 0)
+    {
+        throw new MigrationException("Couldn't find an inital migration.");
+    }
+    else if (initialMigrations.count() > 1)
+    {
+        Migration[string] toCheck;
+        initialMigrations.each!((Migration x) { toCheck[x.id] = x; });
+
+        string initialMigration;
+
+        void checkMigration(Migration m)
+        {
+            // Check if migration replaces another migration
+            if (m.replaces.length > 0)
+            {
+                auto replaces = m.replaces.filter!(x => lookup[x].initial);
+
+                // dfmt off
+                // If no migration has initial = true, throw
+                if (replaces.count() == 0)
+                {
+                    throw new MigrationException(
+                        "Migration with inital = true replaces other migrations"
+                            ~ " without inital = true: " ~ m.id
+                    );
+                }
+                // If more than one migrations have initial = true, throw
+                else if (replaces.count() > 1)
+                {
+                    throw new MigrationException(
+                        "Migration with initial = true replaces other migrations"
+                            ~ " where multiple have initial = true set: " ~ m.id
+                    );
+                }
+
+                // dfmt on
+
+                foreach (mID; m.replaces)
+                {
+                    if (lookup[mID].initial)
+                    {
+                        checkMigration(lookup[mID]);
+                    }
+                }
+            }
+            else
+            {
+                if (initialMigration != "")
+                {
+                    throw new MigrationException(
+                        "There are multiple independent initial migrations: "
+                            ~ initialMigration ~ " : " ~ m.id
+                    );
+                }
+                initialMigration = m.id;
+            }
+
+            // Remove Migration from toCheck list
+            toCheck.remove(m.id);
+        }
+
+        while (toCheck.length != 0)
+        {
+            Migration migration = toCheck.byKeyValue.front.value;
+            checkMigration(migration);
+        }
+
+    }
+}
+
+unittest
+{
+    bool testSelfReferencing()
+    {
+        Migration replaced = Migration(
+            "def", true, "0001_replaced", [], ["0001_replaced"], []
+        );
+
+        Migration[] test;
+        test ~= replaced;
+        try
+        {
+            validateMigrations(test);
+            return false;
+        }
+        catch (MigrationException exc)
+        {
+            return true;
+        }
+    }
+
+    assert(testSelfReferencing() == true);
+}
+
+/** 
  * Intended to get called after conversion.
  *
  * Params:
@@ -121,4 +332,14 @@ Migration[] getExistingMigrations(ref MigrationConfig conf)
 void makeMigrations(SerializedModels serializedModels, MigrationConfig conf)
 {
     Migration[] existing = getExistingMigrations(conf);
+
+    // If there are no existing migrations
+    // the new migration will be the inital one
+    bool inital = existing.length == 0;
+
+    if (!inital)
+    {
+        validateMigrations(existing);
+    }
+
 }
