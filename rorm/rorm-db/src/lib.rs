da19f5cf0c8c620ac23864387b67165f310c50dc
myOmikron <git@omikron.dev>
2022-08-12 03:02:18 +0200

    Added 2 new sub crates

---
+++ rorm/rorm-db/src/lib.rs
@@ -0,0 +1,69 @@
+use sqlx::Error;
+use sqlx::any::AnyPoolOptions;
+use sqlx::mysql::MySqlConnectOptions;
+use sqlx::postgres::PgConnectOptions;
+use sqlx::sqlite::SqliteConnectOptions;
+
+pub enum DatabaseBackend {
+    SQLite,
+    Postgres,
+    MySQL,
+}
+
+pub struct DatabaseConfiguration {
+    pub backend: DatabaseBackend,
+    pub name: String,
+    pub host: String,
+    pub port: u16,
+    pub user: String,
+    pub password: String,
+    pub min_connections: u32,
+    pub max_connections: u32,
+}
+
+pub struct Database {
+    pool: sqlx::Pool<sqlx::Any>,
+}
+
+impl Database {
+    pub async fn connect(configuration: DatabaseConfiguration) -> Result<Self, Error> {
+        let database;
+        let pool_options = AnyPoolOptions::new()
+            .min_connections(configuration.min_connections)
+            .max_connections(configuration.max_connections);
+
+        let pool;
+
+        match configuration.backend {
+            DatabaseBackend::SQLite => {
+                let connect_options = SqliteConnectOptions::new()
+                    .filename(configuration.name);
+                pool = pool_options.connect_with(connect_options.into()).await?;
+            }
+            DatabaseBackend::Postgres => {
+                let connect_options = PgConnectOptions::new()
+                    .host(configuration.host.as_str())
+                    .port(configuration.port)
+                    .username(configuration.user.as_str())
+                    .password(configuration.password.as_str())
+                    .database(configuration.name.as_str());
+                pool = pool_options.connect_with(connect_options.into()).await?;
+            }
+            DatabaseBackend::MySQL => {
+                let connect_options = MySqlConnectOptions::new()
+                    .host(configuration.host.as_str())
+                    .port(configuration.port)
+                    .username(configuration.user.as_str())
+                    .password(configuration.password.as_str())
+                    .database(configuration.name.as_str());
+                pool = pool_options.connect_with(connect_options.into()).await?;
+            }
+        }
+
+        database = Database {
+            pool,
+        };
+
+        return Ok(database);
+    }
+}
