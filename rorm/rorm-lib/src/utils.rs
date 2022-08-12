b83fba529eb28bf8842987803490ce3bf4b8afa1
myOmikron <git@omikron.dev>
2022-08-12 20:41:03 +0200

    FFIString

---
+++ rorm/rorm-lib/src/utils.rs
@@ -0,0 +1,27 @@
+use std::slice::from_raw_parts;
+use std::string::FromUtf8Error;
+
+#[repr(C)]
+pub struct FFIString {
+    content: *const u8,
+    size: usize,
+}
+
+impl TryFrom<FFIString> for String {
+    type Error = FromUtf8Error;
+
+    fn try_from(value: FFIString) -> Result<Self, Self::Error> {
+        unsafe {
+            String::from_utf8(from_raw_parts(value.content, value.size).into())
+        }
+    }
+}
+
+impl From<String> for FFIString {
+    fn from(s: String) -> Self {
+        Self {
+            content: s.as_ptr(),
+            size: s.len(),
+        }
+    }
+}
