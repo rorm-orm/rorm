ca92ced8aeef6c6421d9d6ee6c43c347be265c18
WebFreak001 <gh@webfreak.org>
2022-09-22 00:36:29 +0200

    unittest dorm

--- dorm/source/dorm/lib/util.d
+++ dorm/source/dorm/lib/util.d
@@ -1,8 +1,9 @@
 module dorm.lib.util;
 
 import core.sync.event;
-import std.typecons;
+import std.functional;
 import std.traits;
+import std.typecons;
 
 import dorm.lib.ffi;
 
@@ -43,7 +44,49 @@ struct FreeableAsyncResult(T)
 	}
 }
 
-Event* async_call(alias fn)(Parameters!fn[0 .. $ - 2] args, void delegate(scope Parameters!(Parameters!fn[$ - 1])[1 .. $]) callback)
+auto sync_call(alias fn)(Parameters!fn[0 .. $ - 2] args)
+{
+	static assert(Parameters!(Parameters!fn[$ - 2]).length == 3
+		|| Parameters!(Parameters!fn[$ - 2]).length == 2);
+	static assert(is(Parameters!(Parameters!fn[$ - 2])[0] == void*));
+	static assert(is(Parameters!(Parameters!fn[$ - 2])[$ - 1] == RormError));
+
+	enum isVoid = Parameters!(Parameters!fn[$ - 2]).length == 2;
+
+	struct Result
+	{
+		Exception exception;
+		static if (!isVoid)
+			Parameters!(Parameters!fn[$ - 2])[1] ret;
+		bool sync;
+	}
+
+	Result result;
+
+	extern(C) static void callback(Parameters!(Parameters!fn[$ - 2]) args) nothrow
+	{
+		auto result = cast(Result*)(args[0]);
+		static if (!isVoid)
+			auto data = args[1];
+		auto error = args[$ - 1];
+		if (error) result.exception = error.makeException;
+		else {
+			static if (!isVoid)
+				result.ret = data;
+		}
+		result.sync = true;
+	}
+	fn(forward!args, &callback, &result);
+	assert(result.sync, "called sync_call with function that does not call its callback in synchronous context!");
+
+	if (result.exception)
+		throw result.exception;
+
+	static if (!isVoid)
+		return result.ret;
+}
+
+Event* async_call(alias fn)(Parameters!fn[0 .. $ - 2] args, void delegate(scope Parameters!(Parameters!fn[$ - 2])[1 .. $]) callback)
 {
 	import core.stdc.stdlib;
 	import core.memory;
@@ -54,7 +97,7 @@ Event* async_call(alias fn)(Parameters!fn[0 .. $ - 2] args, void delegate(scope 
 	*(cast(typeof(callback)*)(data + callback.sizeof)) = ret;
 	GC.addRoot(callback.ptr);
 	GC.addRoot(ret);
-	extern(C) static void callback(Parameters!(Parameters!fn[$ - 1]) args) nothrow
+	extern(C) static void callback(Parameters!(Parameters!fn[$ - 2]) args) nothrow
 	{
 		auto event = *cast(Event**)(args[0] + callback.sizeof);
 		auto dg = *cast(typeof(callback)*)args[0];
