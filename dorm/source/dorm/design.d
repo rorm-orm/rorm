b5711386c88b0c60eae5f251a82aefd4673b524f
WebFreak001 <gh@webfreak.org>
2022-09-23 03:26:55 +0200

    minor fixes
    - change copyFiles to preBuildCommands as that didn't seem to work for linking
    - add TimeZone to dorm.design

--- dorm/source/dorm/design.d
+++ dorm/source/dorm/design.d
@@ -1,8 +1,8 @@
 module dorm.design;
 
-public import dorm.model;
+public import core.time;
 public import dorm.annotations;
 public import dorm.declarative.entrypoint;
+public import dorm.model;
+public import std.datetime : Clock, Date, DateTime, SysTime, TimeOfDay, TimeZone;
 public import std.typecons : Nullable;
-public import std.datetime : SysTime, DateTime, Date, TimeOfDay, Clock;
-public import core.time;
