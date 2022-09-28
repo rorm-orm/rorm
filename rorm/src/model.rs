380d2cfd1f2ea55cb8ccb4ff78536d4a96720f8b
gammelalf <gammelalf@hopfen.space>
2022-09-28 18:31:47 +0200

    Added as_primitive method to AsDbType

--- rorm/src/model.rs
+++ rorm/src/model.rs
@@ -1,6 +1,7 @@
 use std::marker::PhantomData;
 use std::ops::{Deref, DerefMut};
 
+use rorm_db::value::Value;
 use rorm_declaration::hmr;
 use rorm_declaration::imr;
 
@@ -18,6 +19,9 @@ pub trait AsDbType {
     /// their decoding without access to the underlying db details (namely `sqlx::Decode`)
     fn from_primitive(primitive: Self::Primitive) -> Self;
 
+    /// Convert a reference to `Self` into the primitive [`Value`] used by our db implementation.
+    fn as_primitive(&self) -> Value;
+
     /// Whether this type supports null.
     const IS_NULLABLE: bool = false;
 
@@ -26,34 +30,44 @@ pub trait AsDbType {
 }
 
 macro_rules! impl_as_db_type {
-    ($type:ty, $variant:ident) => {
+    ($type:ty, $db_type:ident, $value_variant:ident $(using $method:ident)?) => {
         impl AsDbType for $type {
             type Primitive = Self;
 
-            type DbType = hmr::$variant;
+            type DbType = hmr::$db_type;
 
             #[inline(always)]
             fn from_primitive(primitive: Self::Primitive) -> Self {
                 primitive
             }
+
+            impl_as_db_type!(impl_as_primitive, $type, $db_type, $value_variant $(using $method)?);
+        }
+    };
+    (impl_as_primitive, $type:ty, $db_type:ident, $value_variant:ident) => {
+        #[inline(always)]
+        fn as_primitive(&self) -> Value {
+            Value::$value_variant(*self)
+        }
+    };
+    (impl_as_primitive, $type:ty, $db_type:ident, $value_variant:ident using $method:ident) => {
+        #[inline(always)]
+        fn as_primitive(&self) -> Value {
+            Value::$value_variant(self.$method())
         }
     };
 }
-impl_as_db_type!(chrono::NaiveTime, Time);
-impl_as_db_type!(chrono::NaiveDateTime, DateTime);
-impl_as_db_type!(chrono::NaiveDate, Date);
-impl_as_db_type!(Vec<u8>, VarBinary);
-impl_as_db_type!(i8, Int8);
-impl_as_db_type!(i16, Int16);
-impl_as_db_type!(i32, Int32);
-impl_as_db_type!(i64, Int64);
-impl_as_db_type!(u8, UInt8);
-impl_as_db_type!(u16, UInt16);
-impl_as_db_type!(u32, UInt32);
-impl_as_db_type!(f32, Float);
-impl_as_db_type!(f64, Double);
-impl_as_db_type!(bool, Boolean);
-impl_as_db_type!(String, VarChar);
+impl_as_db_type!(chrono::NaiveTime, Time, NaiveTime);
+impl_as_db_type!(chrono::NaiveDateTime, DateTime, NaiveDateTime);
+impl_as_db_type!(chrono::NaiveDate, Date, NaiveDate);
+impl_as_db_type!(i16, Int16, I16);
+impl_as_db_type!(i32, Int32, I32);
+impl_as_db_type!(i64, Int64, I64);
+impl_as_db_type!(f32, Float, F32);
+impl_as_db_type!(f64, Double, F64);
+impl_as_db_type!(bool, Boolean, Bool);
+impl_as_db_type!(Vec<u8>, VarBinary, Binary using as_slice);
+impl_as_db_type!(String, VarChar, String using as_str);
 impl<T: AsDbType> AsDbType for Option<T> {
     type Primitive = Self;
     type DbType = T::DbType;
@@ -61,6 +75,13 @@ impl<T: AsDbType> AsDbType for Option<T> {
     #[inline(always)]
     fn from_primitive(primitive: Self::Primitive) -> Self {
         primitive
+    }
+
+    fn as_primitive(&self) -> Value {
+        match self {
+            Some(value) => value.as_primitive(),
+            None => Value::Null,
+        }
     }
 
     const IS_NULLABLE: bool = true;
@@ -102,6 +123,10 @@ impl<E: DbEnum> AsDbType for E {
 
     fn from_primitive(primitive: Self::Primitive) -> Self {
         E::from_str(&primitive)
+    }
+
+    fn as_primitive(&self) -> Value {
+        Value::String(self.to_str())
     }
 
     fn implicit_annotations(annotations: &mut Vec<imr::Annotation>) {
@@ -171,6 +196,10 @@ impl<I: AsDbType> AsDbType for GenericId<I> {
 
     fn from_primitive(primitive: Self::Primitive) -> Self {
         GenericId(primitive)
+    }
+
+    fn as_primitive(&self) -> Value {
+        self.0.as_primitive()
     }
 
     fn implicit_annotations(annotations: &mut Vec<imr::Annotation>) {
