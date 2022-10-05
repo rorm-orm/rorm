9d99b058d311f5380f360e8eb48a1ccfc9c92ec7
myOmikron <git@omikron.dev>
2022-10-05 22:12:28 +0200

    Implemented custom PartialEq and Hash for Annotation

--- rorm/rorm-declaration/src/imr.rs
+++ rorm/rorm-declaration/src/imr.rs
@@ -1,4 +1,6 @@
 //! The Internal Model Representation used by our migration cli tool
+use std::hash::{Hash, Hasher};
+
 use ordered_float::OrderedFloat;
 use serde::{Deserialize, Serialize};
 
@@ -90,7 +92,7 @@ pub enum DbType {
 
 /// The subset of annotations which need to be communicated with the migration tool
 #[non_exhaustive]
-#[derive(Serialize, Deserialize, Debug, Clone, Hash)]
+#[derive(Serialize, Deserialize, Debug, Clone, Eq)]
 #[serde(tag = "Type", content = "Value")]
 #[serde(rename_all = "snake_case")]
 pub enum Annotation {
@@ -116,6 +118,101 @@ pub enum Annotation {
     PrimaryKey,
     /// UNIQUE constraint
     Unique,
+}
+
+impl PartialEq for Annotation {
+    fn eq(&self, other: &Self) -> bool {
+        match (self, other) {
+            (Annotation::AutoUpdateTime, Annotation::AutoCreateTime) => true,
+            (Annotation::AutoIncrement, Annotation::AutoIncrement) => true,
+            (Annotation::Choices(_), Annotation::Choices(_)) => true,
+            (Annotation::DefaultValue(_), Annotation::DefaultValue(_)) => true,
+            (Annotation::Index(_), Annotation::Index(_)) => true,
+            (Annotation::MaxLength(_), Annotation::MaxLength(_)) => true,
+            (Annotation::NotNull, Annotation::NotNull) => true,
+            (Annotation::PrimaryKey, Annotation::PrimaryKey) => true,
+            (Annotation::Unique, Annotation::Unique) => true,
+            _ => false,
+        }
+    }
+
+    fn ne(&self, other: &Self) -> bool {
+        match (self, other) {
+            (Annotation::AutoUpdateTime, Annotation::AutoCreateTime) => false,
+            (Annotation::AutoIncrement, Annotation::AutoIncrement) => false,
+            (Annotation::Choices(_), Annotation::Choices(_)) => false,
+            (Annotation::DefaultValue(_), Annotation::DefaultValue(_)) => false,
+            (Annotation::Index(_), Annotation::Index(_)) => false,
+            (Annotation::MaxLength(_), Annotation::MaxLength(_)) => false,
+            (Annotation::NotNull, Annotation::NotNull) => false,
+            (Annotation::PrimaryKey, Annotation::PrimaryKey) => false,
+            (Annotation::Unique, Annotation::Unique) => false,
+            _ => true,
+        }
+    }
+}
+
+impl Hash for Annotation {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        match self {
+            Annotation::AutoCreateTime => state.write_i8(0),
+            Annotation::AutoUpdateTime => state.write_i8(1),
+            Annotation::AutoIncrement => state.write_i8(2),
+            Annotation::Choices(_) => state.write_i8(3),
+            Annotation::DefaultValue(_) => state.write_i8(4),
+            Annotation::Index(_) => state.write_i8(5),
+            Annotation::MaxLength(_) => state.write_i8(6),
+            Annotation::NotNull => state.write_i8(7),
+            Annotation::PrimaryKey => state.write_i8(8),
+            Annotation::Unique => state.write_i8(9),
+        }
+    }
+
+    fn hash_slice<H: Hasher>(data: &[Self], state: &mut H)
+    where
+        Self: Sized,
+    {
+        for x in data {
+            x.hash(state);
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::imr::{Annotation, IndexValue};
+    use std::collections::hash_map::DefaultHasher;
+    use std::hash::{Hash, Hasher};
+
+    #[test]
+    fn test_annotation_hash() {
+        let mut hasher1 = DefaultHasher::new();
+        let mut hasher2 = DefaultHasher::new();
+
+        Annotation::MaxLength(1).hash(&mut hasher1);
+        Annotation::MaxLength(12313).hash(&mut hasher2);
+
+        Annotation::Index(None).hash(&mut hasher1);
+        Annotation::Index(Some(IndexValue {
+            priority: None,
+            name: "foo".to_string(),
+        }))
+        .hash(&mut hasher2);
+
+        assert_eq!(hasher1.finish(), hasher2.finish());
+    }
+
+    #[test]
+    fn test_annotation_partial_eq() {
+        assert_eq!(Annotation::MaxLength(1), Annotation::MaxLength(2));
+        assert_eq!(
+            Annotation::Index(None),
+            Annotation::Index(Some(IndexValue {
+                priority: None,
+                name: "foo".to_string()
+            }))
+        );
+    }
 }
 
 /// Represents a complex index
