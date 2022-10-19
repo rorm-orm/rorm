26958969a2ee8f359308e3c6a8ef7a1c3a564304
gammelalf <gammelalf@hopfen.space>
2022-10-19 22:41:13 +0200

    Added transactions to crud builders

--- rorm/src/crud/builder.rs
+++ rorm/src/crud/builder.rs
@@ -9,6 +9,7 @@ pub(crate) mod private {
     pub trait Private {}
 }
 use private::Private;
+use rorm_db::transaction::Transaction;
 
 /// This trait can not be implemented by foreign packages.
 ///
@@ -17,6 +18,7 @@ pub trait Sealed {
     #[doc(hidden)]
     fn sealed<P: Private>() {}
 }
+impl Sealed for () {}
 
 /// Marker for the generic parameter storing a db [Condition]
 pub trait ConditionMarker<'a>: Sealed + 'a {
@@ -24,16 +26,32 @@ pub trait ConditionMarker<'a>: Sealed + 'a {
     fn as_option(&self) -> Option<&Condition<'a>>;
 }
 
-impl Sealed for () {}
+impl<'a> Sealed for Condition<'a> {}
+impl<'a> ConditionMarker<'a> for Condition<'a> {
+    fn as_option(&self) -> Option<&Condition<'a>> {
+        Some(self)
+    }
+}
 impl<'a> ConditionMarker<'a> for () {
     fn as_option(&self) -> Option<&Condition<'a>> {
         None
     }
 }
 
-impl<'a> Sealed for Condition<'a> {}
-impl<'a> ConditionMarker<'a> for Condition<'a> {
-    fn as_option(&self) -> Option<&Condition<'a>> {
+/// Marker for the generic parameter storing a db [Transaction]
+pub trait TransactionMarker<'rf, 'db>: Sealed + 'rf {
+    /// Convert the generic transaction into [Option] expected by [rorm_db]
+    fn into_option(self) -> Option<&'rf mut Transaction<'db>>;
+}
+
+impl<'rf, 'db: 'rf> Sealed for &'rf mut Transaction<'db> {}
+impl<'rf, 'db: 'rf> TransactionMarker<'rf, 'db> for &'rf mut Transaction<'db> {
+    fn into_option(self) -> Option<&'rf mut Transaction<'db>> {
         Some(self)
+    }
+}
+impl<'rf, 'db> TransactionMarker<'rf, 'db> for () {
+    fn into_option(self) -> Option<&'rf mut Transaction<'db>> {
+        None
     }
 }
--- rorm/src/crud/query.rs
+++ rorm/src/crud/query.rs
@@ -3,9 +3,10 @@ use std::marker::PhantomData;
 
 use futures::stream::BoxStream;
 use futures::{Stream, StreamExt};
+use rorm_db::transaction::Transaction;
 use rorm_db::{conditional::Condition, error::Error, row::Row, Database};
 
-use crate::crud::builder::ConditionMarker;
+use crate::crud::builder::{ConditionMarker, TransactionMarker};
 use crate::model::{Model, Patch};
 
 /// Marker for type a [QueryBuilder]'s result should be converted to.
@@ -16,115 +17,166 @@ impl<M: Model> QueryResult<M> for () {}
 impl<M: Model, P: Patch<Model = M>> QueryResult<M> for P {}
 
 /// Builder for creating queries
-///
-/// 1. Start by specifying which columns to select:
-///     - [`QueryBuilder::select_columns`] to select columns manually
-///     - [`QueryBuilder::select_model`] to select all columns the model has
-///     - [`QueryBuilder::select_patch`] to select all columns a patch has
-/// 2. Add optional modifiers:
-///     - [`QueryBuilder::condition`]
-/// 3. Retrieve rows from the db:
-///     - [`QueryBuilder::all`] to get all rows at once
-///     - [`QueryBuilder::stream`] to get all rows in a stream
-///     - [`QueryBuilder::one`] to get a single row
-///     - [`QueryBuilder::optional`] to get an optional single row
-pub struct QueryBuilder<'a, M: Model, R: QueryResult<M>, C: ConditionMarker<'a>> {
-    db: &'a Database,
+pub struct QueryBuilder<
+    'db,
+    'a,
+    M: Model,
+    R: QueryResult<M>,
+    C: ConditionMarker<'a>,
+    T: TransactionMarker<'a, 'db>,
+> {
+    db: &'db Database,
     columns: &'a [&'a str],
     _phantom: PhantomData<(M, R)>,
 
     condition: C,
+    transaction: T,
 }
 
-impl<'a, M: Model> QueryBuilder<'a, M, (), ()> {
+impl<'db, 'a, M: Model> QueryBuilder<'db, 'a, M, (), (), ()> {
     /// Start building a query which selects a given set of columns
-    pub fn select_columns(db: &'a Database, columns: &'a [&'a str]) -> Self {
+    pub fn select_columns(db: &'db Database, columns: &'a [&'a str]) -> Self {
         QueryBuilder {
             db,
             columns, // TODO: check for existence
             _phantom: PhantomData,
 
             condition: (),
+            transaction: (),
         }
     }
 }
 
-impl<'a, M: Model, P: Patch<Model = M>> QueryBuilder<'a, M, P, ()> {
+impl<'db, 'a, M: Model, P: Patch<Model = M>> QueryBuilder<'db, 'a, M, P, (), ()> {
     /// Start building a query which selects a patch's columns
-    pub fn select_patch(db: &'a Database) -> Self {
+    pub fn select_patch(db: &'db Database) -> Self {
         QueryBuilder {
             db,
             columns: P::COLUMNS,
             _phantom: PhantomData,
 
             condition: (),
+            transaction: (),
         }
     }
 }
 
-impl<'a, M: Model> QueryBuilder<'a, M, M, ()> {
+impl<'db, 'a, M: Model> QueryBuilder<'db, 'a, M, M, (), ()> {
     /// Start building a query which selects all columns
-    pub fn select_model(db: &'a Database) -> Self {
+    pub fn select_model(db: &'db Database) -> Self {
         QueryBuilder {
             db,
             columns: M::COLUMNS,
             _phantom: PhantomData,
 
             condition: (),
+            transaction: (),
         }
     }
 }
 
-impl<'a, M: Model, R: QueryResult<M>> QueryBuilder<'a, M, R, ()> {
+impl<'db, 'a, M: Model, R: QueryResult<M>, T: TransactionMarker<'a, 'db>>
+    QueryBuilder<'db, 'a, M, R, (), T>
+{
     /// Add a condition to the query
-    pub fn condition(&self, condition: Condition<'a>) -> QueryBuilder<'a, M, R, Condition<'a>> {
-        QueryBuilder {
-            db: self.db,
-            columns: self.columns,
-            _phantom: self._phantom,
+    pub fn condition(
+        self,
+        condition: Condition<'a>,
+    ) -> QueryBuilder<'db, 'a, M, R, Condition<'a>, T> {
+        #[rustfmt::skip]
+        let QueryBuilder { db, columns, _phantom, transaction, .. } = self;
+        #[rustfmt::skip]
+        return QueryBuilder { db, columns, _phantom, condition, transaction, };
+    }
+}
 
-            condition,
-        }
+impl<'db, 'a, M: Model, R: QueryResult<M>, C: ConditionMarker<'a>>
+    QueryBuilder<'db, 'a, M, R, C, ()>
+{
+    /// Add a transaction to the query
+    pub fn transaction(
+        self,
+        transaction: &'a mut Transaction<'db>,
+    ) -> QueryBuilder<'db, 'a, M, R, C, &'a mut Transaction<'db>> {
+        #[rustfmt::skip]
+        let QueryBuilder { db, columns, _phantom, condition, .. } = self;
+        #[rustfmt::skip]
+        return QueryBuilder { db, columns, _phantom, condition, transaction, };
     }
 }
 
 // Execute query and return rows
 // This doesn't require any specific QueryResult
-impl<'a, M: Model, R: QueryResult<M>, C: ConditionMarker<'a>> QueryBuilder<'a, M, R, C> {
+impl<
+        'rf,
+        'db: 'rf,
+        M: Model,
+        R: QueryResult<M>,
+        C: ConditionMarker<'rf>,
+        T: TransactionMarker<'rf, 'db>,
+    > QueryBuilder<'db, 'rf, M, R, C, T>
+{
     /// Retrieve all matching rows
-    pub async fn all_as_rows(&self) -> Result<Vec<Row>, Error> {
+    pub async fn all_as_rows(self) -> Result<Vec<Row>, Error> {
         self.db
-            .query_all(M::table_name(), self.columns, self.condition.as_option())
+            .query_all(
+                M::table_name(),
+                self.columns,
+                self.condition.as_option(),
+                self.transaction.into_option(),
+            )
             .await
     }
 
     /// Retrieve exactly one matching row
     ///
     /// An error is returned if no value could be retrieved.
-    pub async fn one_as_row(&self) -> Result<Row, Error> {
+    pub async fn one_as_row(self) -> Result<Row, Error> {
         self.db
-            .query_one(M::table_name(), self.columns, self.condition.as_option())
+            .query_one(
+                M::table_name(),
+                self.columns,
+                self.condition.as_option(),
+                self.transaction.into_option(),
+            )
             .await
     }
 
     /// Retrieve the query as a stream of rows
-    pub fn stream_as_row(&self) -> BoxStream<'a, Result<Row, Error>> {
-        self.db
-            .query_stream(M::table_name(), self.columns, self.condition.as_option())
+    pub fn stream_as_row(self) -> BoxStream<'rf, Result<Row, Error>> {
+        self.db.query_stream(
+            M::table_name(),
+            self.columns,
+            self.condition.as_option(),
+            self.transaction.into_option(),
+        )
     }
 
     /// Try to retrieve the a matching row
-    pub async fn optional_as_row(&self) -> Result<Option<Row>, Error> {
+    pub async fn optional_as_row(self) -> Result<Option<Row>, Error> {
         self.db
-            .query_optional(M::table_name(), self.columns, self.condition.as_option())
+            .query_optional(
+                M::table_name(),
+                self.columns,
+                self.condition.as_option(),
+                self.transaction.into_option(),
+            )
             .await
     }
 }
 
 // Execute query and map result through TryFrom<Row>
-impl<'a, M: Model, P: Patch<Model = M>, C: ConditionMarker<'a>> QueryBuilder<'a, M, P, C> {
+impl<
+        'db: 'rf,
+        'rf,
+        M: Model,
+        P: Patch<Model = M>,
+        C: ConditionMarker<'rf>,
+        T: TransactionMarker<'rf, 'db>,
+    > QueryBuilder<'db, 'rf, M, P, C, T>
+{
     /// Retrieve all matching rows as unpacked models
-    pub async fn all(&self) -> Result<Vec<P>, Error> {
+    pub async fn all(self) -> Result<Vec<P>, Error> {
         self.all_as_rows()
             .await?
             .into_iter()
@@ -137,20 +189,20 @@ impl<'a, M: Model, P: Patch<Model = M>, C: ConditionMarker<'a>> QueryBuilder<'a,
     /// Retrieve exactly one matching row as Model
     ///
     /// An error is returned if no value could be retrieved.
-    pub async fn one(&self) -> Result<P, Error> {
+    pub async fn one(self) -> Result<P, Error> {
         P::try_from(self.one_as_row().await?)
             .map_err(|_| Error::DecodeError("Could not decode row".to_string()))
     }
 
     /// Retrieve the query as a stream of Models
-    pub fn stream(&self) -> impl Stream<Item = Result<P, Error>> + 'a {
+    pub fn stream(self) -> impl Stream<Item = Result<P, Error>> + 'rf {
         self.stream_as_row().map(|row| {
             P::try_from(row?).map_err(|_| Error::DecodeError("Could not decode row".to_string()))
         })
     }
 
     /// Try to retrieve the a matching row as Model
-    pub async fn optional(&self) -> Result<Option<P>, Error> {
+    pub async fn optional(self) -> Result<Option<P>, Error> {
         match self.optional_as_row().await? {
             None => Ok(None),
             Some(row) => {
@@ -166,6 +218,6 @@ impl<'a, M: Model, P: Patch<Model = M>, C: ConditionMarker<'a>> QueryBuilder<'a,
 #[macro_export]
 macro_rules! query {
     ($db:expr, $patch:path) => {
-        $crate::QueryBuilder::select_patch::<$patch>(&$db)
+        $crate::crud::query::QueryBuilder::<_, $patch, _, _>::select_patch($db)
     };
 }
