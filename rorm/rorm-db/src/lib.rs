6d839a85ded7c68f202cfd03cd868dcd55292bfb
myOmikron <git@omikron.dev>
2022-11-02 00:58:28 +0100

    Updated project to 0.2.0

--- rorm/rorm-db/src/lib.rs
+++ rorm/rorm-db/src/lib.rs
@@ -4,32 +4,149 @@
 //! `rorm-lib` implements C bindings for this crate.
 #![warn(missing_docs)]
 
+#[cfg(any(
+    all(
+        feature = "actix-rustls",
+        any(
+            feature = "actix-native-tls",
+            feature = "tokio-native-tls",
+            feature = "tokio-rustls",
+            feature = "async-std-native-tls",
+            feature = "async-std-rustls"
+        )
+    ),
+    all(
+        feature = "actix-native-tls",
+        any(
+            feature = "tokio-native-tls",
+            feature = "tokio-rustls",
+            feature = "async-std-native-tls",
+            feature = "async-std-rustls"
+        )
+    ),
+    all(
+        feature = "tokio-rustls",
+        any(
+            feature = "tokio-native-tls",
+            feature = "async-std-native-tls",
+            feature = "async-std-rustls"
+        )
+    ),
+    all(
+        feature = "tokio-native-tls",
+        any(feature = "async-std-native-tls", feature = "async-std-rustls")
+    ),
+    all(feature = "async-std-native-tls", feature = "async-std-rustls")
+))]
+compile_error!("Using multiple runtime / tls configurations at the same time is not allowed");
+
+#[cfg(not(any(
+    feature = "async-std-native-tls",
+    feature = "async-std-rustls",
+    feature = "tokio-native-tls",
+    feature = "tokio-rustls",
+    feature = "actix-native-tls",
+    feature = "actix-rustls"
+)))]
+compile_error!(
+    r#"One of async-std-native-tls, async-std-rustls, tokio-native-tls, tokio-rustls, 
+    actix-native-tls, actix-rustls is required"#
+);
+
+#[cfg(any(
+    feature = "async-std-native-tls",
+    feature = "async-std-rustls",
+    feature = "tokio-native-tls",
+    feature = "tokio-rustls",
+    feature = "actix-native-tls",
+    feature = "actix-rustls"
+))]
 pub mod database;
 /**
 Errors of rorm-db will be specified here.
-*/
+ */
+#[cfg(any(
+    feature = "async-std-native-tls",
+    feature = "async-std-rustls",
+    feature = "tokio-native-tls",
+    feature = "tokio-rustls",
+    feature = "actix-native-tls",
+    feature = "actix-rustls"
+))]
 pub mod error;
 /**
 This module holds the results of a query
-*/
+ */
+#[cfg(any(
+    feature = "async-std-native-tls",
+    feature = "async-std-rustls",
+    feature = "tokio-native-tls",
+    feature = "tokio-rustls",
+    feature = "actix-native-tls",
+    feature = "actix-rustls"
+))]
 pub mod result;
+#[cfg(any(
+    feature = "async-std-native-tls",
+    feature = "async-std-rustls",
+    feature = "tokio-native-tls",
+    feature = "tokio-rustls",
+    feature = "actix-native-tls",
+    feature = "actix-rustls"
+))]
 pub mod row;
 /// This module holds the definition of transactions
+#[cfg(any(
+    feature = "async-std-native-tls",
+    feature = "async-std-rustls",
+    feature = "tokio-native-tls",
+    feature = "tokio-rustls",
+    feature = "actix-native-tls",
+    feature = "actix-rustls"
+))]
 pub mod transaction;
 /// Utility functions
+#[cfg(any(
+    feature = "async-std-native-tls",
+    feature = "async-std-rustls",
+    feature = "tokio-native-tls",
+    feature = "tokio-rustls",
+    feature = "actix-native-tls",
+    feature = "actix-rustls"
+))]
 pub mod utils;
 
-pub use rorm_sql::conditional;
-pub use rorm_sql::value;
-pub use rorm_sql::{and, or};
+#[cfg(any(
+    feature = "async-std-native-tls",
+    feature = "async-std-rustls",
+    feature = "tokio-native-tls",
+    feature = "tokio-rustls",
+    feature = "actix-native-tls",
+    feature = "actix-rustls"
+))]
+pub use rorm_sql::{and, conditional, or, value};
 
-pub use crate::database::Database;
-pub use crate::error::Error;
-pub use crate::row::Row;
+#[cfg(any(
+    feature = "async-std-native-tls",
+    feature = "async-std-rustls",
+    feature = "tokio-native-tls",
+    feature = "tokio-rustls",
+    feature = "actix-native-tls",
+    feature = "actix-rustls"
+))]
+pub use crate::{database::Database, error::Error, row::Row};
 
 /**
 Representation of different backends
-*/
+ */
+#[cfg(any(
+    feature = "async-std-native-tls",
+    feature = "async-std-rustls",
+    feature = "tokio-native-tls",
+    feature = "tokio-rustls",
+    feature = "actix-native-tls",
+    feature = "actix-rustls"
+))]
 pub enum DatabaseBackend {
     /// SQLite database backend
     SQLite,
@@ -50,7 +167,15 @@ database to connect to.
 
 `min_connections` and `max_connections` must be greater than 0
 and `max_connections` must be greater or equals `min_connections`.
-*/
+ */
+#[cfg(any(
+    feature = "async-std-native-tls",
+    feature = "async-std-rustls",
+    feature = "tokio-native-tls",
+    feature = "tokio-rustls",
+    feature = "actix-native-tls",
+    feature = "actix-rustls"
+))]
 pub struct DatabaseConfiguration {
     /// Specifies the driver that will be used
     pub backend: DatabaseBackend,
