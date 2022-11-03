0e1cbebfc6b8796c7520a25803f24be188a31958
Chris <chris@hopfen.space>
2022-11-03 04:18:49 +0100

    Added an operation module

--- rorm/rorm-sample/src/main.rs
+++ rorm/rorm-sample/src/main.rs
@@ -1,55 +1,44 @@
+use std::fs::read_to_string;
+
 use clap::Parser;
-use rorm::DatabaseConfiguration;
+use rorm::{config::DatabaseConfig, Database, DatabaseConfiguration};
 use serde::{Deserialize, Serialize};
-use std::path::PathBuf;
-use std::process::exit;
 use toml;
+
+mod operations;
 
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "PascalCase")]
 pub struct ConfigFile {
-    pub database: rorm::config::DatabaseConfig,
+    pub database: DatabaseConfig,
 }
 
 #[derive(Parser)]
 struct Cli {
     /// Specify the database configuration file
-    config_file: Option<PathBuf>,
+    config_file: String,
 }
 
 #[rorm::rorm_main]
 #[tokio::main]
-async fn main() {
+async fn main() -> anyhow::Result<()> {
     // Enable the environment logger
     env_logger::init();
 
     // Get the config file from the CLI arguments
-    let cli: Cli = Cli::parse();
-    let path;
-    match cli.config_file {
-        None => {
-            eprintln!("The mandatory argument 'config_file' is missing");
-            exit(1);
-        }
-        Some(p) => path = p.to_str().unwrap().to_string(),
-    }
+    let path = Cli::parse().config_file;
 
     // Read the config from a TOML file
-    let db_conf_file = toml::from_str::<ConfigFile>(
-        std::fs::read_to_string(&path)
-            .expect("File read error")
-            .as_str(),
-    )
-    .expect("Couldn't deserialize configuration file");
+    let db_conf_file: ConfigFile = toml::from_str(&read_to_string(&path)?)?;
 
     // Connect to the database to get the database handle using the TOML configuration
-    let db = rorm::Database::connect(DatabaseConfiguration {
+    let db = Database::connect(DatabaseConfiguration {
         driver: db_conf_file.database.driver,
         min_connections: 1,
         max_connections: 1,
     })
-    .await
-    .expect("error connecting to the database");
+    .await?;
 
-    // TODO
+    // Perform project-specific operations on the database
+    operations::operate(db).await
 }
