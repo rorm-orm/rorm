14652d5b4d4c7ae62a61f583d0bf0d25f5bf598a
gammelalf <gammelalf@hopfen.space>
2026-08-04 20:59:55 +0200

    Allowed handling of deprecated variants

--- src/insert.rs
+++ src/insert.rs
@@ -124,6 +124,7 @@ impl<'post_build> Insert<'post_build> for InsertImpl<'_, 'post_build> {
                     write!(s, "(").unwrap();
                     for (idx_2, y) in x.iter().enumerate() {
                         match y {
+                            #[allow(deprecated)]
                             Value::Ident(st) => write!(s, "\"{}\"", *st).unwrap(),
                             Value::Choice(c) => write!(s, "{}", sqlite::fmt(c)).unwrap(),
                             Value::Null(NullType::Choice) => write!(s, "NULL").unwrap(),
