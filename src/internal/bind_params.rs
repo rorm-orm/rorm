d9d3320aed2be74e02fc6bdda89d2dfa2416aacc
gammelalf <gammelalf@hopfen.space>
2026-08-04 20:56:07 +0200

    Handle new postgres-only array values

--- src/internal/bind_params.rs
+++ src/internal/bind_params.rs
@@ -47,6 +47,53 @@ where
         #[cfg(feature = "postgres-only")]
         Value::BitVec(x) => query.bind(x),
 
+        #[cfg(feature = "postgres-only")]
+        Value::ArrayString(x) => query.bind(x),
+        #[cfg(feature = "postgres-only")]
+        Value::ArrayI64(x) => query.bind(x),
+        #[cfg(feature = "postgres-only")]
+        Value::ArrayI32(x) => query.bind(x),
+        #[cfg(feature = "postgres-only")]
+        Value::ArrayI16(x) => query.bind(x),
+        #[cfg(feature = "postgres-only")]
+        Value::ArrayBool(x) => query.bind(x),
+        #[cfg(feature = "postgres-only")]
+        Value::ArrayF32(x) => query.bind(x),
+        #[cfg(feature = "postgres-only")]
+        Value::ArrayF64(x) => query.bind(x),
+        #[cfg(feature = "postgres-only")]
+        Value::ArrayBinary(x) => query.bind(x),
+        #[cfg(feature = "postgres-only")]
+        Value::ArrayChronoNaiveDate(x) => query.bind(x),
+        #[cfg(feature = "postgres-only")]
+        Value::ArrayChronoNaiveTime(x) => query.bind(x),
+        #[cfg(feature = "postgres-only")]
+        Value::ArrayChronoNaiveDateTime(x) => query.bind(x),
+        #[cfg(feature = "postgres-only")]
+        Value::ArrayChronoDateTime(x) => query.bind(x),
+
+        #[cfg(feature = "postgres-only")]
+        Value::ArrayTimeDate(x) => query.bind(x),
+        #[cfg(feature = "postgres-only")]
+        Value::ArrayTimeTime(x) => query.bind(x),
+        #[cfg(feature = "postgres-only")]
+        Value::ArrayTimeOffsetDateTime(x) => query.bind(x),
+        #[cfg(feature = "postgres-only")]
+        Value::ArrayTimePrimitiveDateTime(x) => query.bind(x),
+
+        #[cfg(feature = "postgres-only")]
+        Value::ArrayUuid(x) => query.bind(x),
+
+        #[cfg(feature = "postgres-only")]
+        Value::ArrayJsonValue(x) => query.bind(Json(x)),
+
+        #[cfg(feature = "postgres-only")]
+        Value::ArrayMacAddress(x) => query.bind(x),
+        #[cfg(feature = "postgres-only")]
+        Value::ArrayIpNetwork(x) => query.bind(x),
+        #[cfg(feature = "postgres-only")]
+        Value::ArrayBitVec(x) => query.bind(x),
+
         Value::Null(null_type) => match null_type {
             NullType::String => query.bind(None::<&str>),
             NullType::I64 => query.bind(None::<i64>),
@@ -81,6 +128,40 @@ where
             #[cfg(feature = "postgres-only")]
             NullType::BitVec => query.bind(none(Value::BitVec)),
         },
+        #[cfg(feature = "postgres-only")]
+        Value::ArrayNull(null_type) => match null_type {
+            NullType::String => query.bind(None::<&str>),
+            NullType::I64 => query.bind(None::<i64>.map(vec)),
+            NullType::I32 => query.bind(None::<i32>.map(vec)),
+            NullType::I16 => query.bind(None::<i16>.map(vec)),
+            NullType::Bool => query.bind(None::<bool>.map(vec)),
+            NullType::F64 => query.bind(None::<f64>.map(vec)),
+            NullType::F32 => query.bind(None::<f32>.map(vec)),
+            NullType::Binary => query.bind(None::<&[u8]>.map(vec)),
+            NullType::Choice => {}
+
+            NullType::ChronoNaiveTime => query.bind(none(Value::ChronoNaiveTime).map(vec)),
+            NullType::ChronoNaiveDate => query.bind(none(Value::ChronoNaiveDate).map(vec)),
+            NullType::ChronoNaiveDateTime => query.bind(none(Value::ChronoNaiveDateTime).map(vec)),
+            NullType::ChronoDateTime => query.bind(none(Value::ChronoDateTime).map(vec)),
+
+            NullType::TimeDate => query.bind(none(Value::TimeDate).map(vec)),
+            NullType::TimeTime => query.bind(none(Value::TimeTime).map(vec)),
+            NullType::TimeOffsetDateTime => query.bind(none(Value::TimeOffsetDateTime).map(vec)),
+            NullType::TimePrimitiveDateTime => {
+                query.bind(none(Value::TimePrimitiveDateTime).map(vec))
+            }
+
+            NullType::Uuid => query.bind(none(Value::Uuid).map(vec)),
+            NullType::UuidHyphenated => query.bind(None::<String>.map(vec)),
+            NullType::UuidSimple => query.bind(None::<String>.map(vec)),
+
+            NullType::JsonValue => query.bind(none(Value::JsonValue).map(vec)),
+
+            NullType::MacAddress => query.bind(none(Value::MacAddress).map(vec)),
+            NullType::IpNetwork => query.bind(none(Value::IpNetwork).map(vec)),
+            NullType::BitVec => query.bind(none(Value::BitVec).map(vec)),
+        },
     }
 }
 
@@ -91,4 +172,11 @@ where
     Option<T>: 'a + Send + AnyEncode<'a> + AnyType,
 {
     None
+}
+
+/// Hack to avoid naming the types which would require direct dependencies
+///
+/// Used to convert an `Option<T>` which is **always** `None` with `.map()`
+fn vec<T>(_never: T) -> Vec<T> {
+    unreachable!()
 }
