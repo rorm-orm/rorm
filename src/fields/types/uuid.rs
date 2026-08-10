111e5f20476497a912e871779ba675296ab4ea4d
myOmikron <git@omikron.dev>
2026-08-11 00:45:30 +0200

    Implemented SimpleFieldOrd for Uuid

--- src/fields/types/uuid.rs
+++ src/fields/types/uuid.rs
@@ -2,7 +2,7 @@ use uuid::Uuid;
 
 use crate::conditions::Value;
 use crate::fields::traits::into_value::IntoValue;
-use crate::fields::traits::simple::{SimpleFieldEq, SimpleFieldIn};
+use crate::fields::traits::simple::{SimpleFieldEq, SimpleFieldIn, SimpleFieldOrd};
 use crate::impl_FieldType;
 
 impl_FieldType!(Uuid, Uuid);
@@ -13,3 +13,4 @@ impl<'a> IntoValue<'a> for Uuid {
 }
 impl SimpleFieldEq<Uuid> for Uuid {}
 impl SimpleFieldIn<Uuid> for Uuid {}
+impl SimpleFieldOrd<Uuid> for Uuid {}
